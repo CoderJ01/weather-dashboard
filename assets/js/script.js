56991f2619c83c4e658fba7a406235c05f3d1f8c
Coder J <buildingguy@sbcglobal.net>
2021-12-20 17:44:55 -0800

    Testing123: fetch api (2nd method)

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,27 @@
-fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&&units=imperial&lon=-94.04&exclude=hourly&appid=3f2e409528a52056b62087fa623591ee')
-  .then(response => response.json())
-  .then(data => console.log(data));
+function infoChicago () {
+    // fetch('https://api.openweathermap.org/data/2.5/onecall?lat=33.44&&units=imperial&lon=-94.04&exclude=hourly&appid=3f2e409528a52056b62087fa623591ee')
+    // .then(response => response.json())
+    // .then(data => console.log(data));
 
+    var chicagoWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&&units=imperial&lon=-94.04&exclude=hourly&appid=3f2e409528a52056b62087fa623591ee';
+
+    fetch(chicagoWeather)
+    .then(function(response) {
+    // request was successful
+    if (response.ok) {
+        console.log(response);
+        response.json().then(function(data) {
+        console.log(data);
+        });
+    } else {
+        alert("Error: " + response.statusText);
+    }
+    })
+    .catch(function(error) {
+    alert("Unable to connect to OpenWeather");
+    });
+}
+
+infoChicago();
 
 
