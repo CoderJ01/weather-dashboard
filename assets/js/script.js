af1c61919b3084385f77a2f847d0fd07d4a7d0b5
Coder J <buildingguy@sbcglobal.net>
2021-12-20 19:22:35 -0800

    set up html for 5-day forecast

--- assets/js/script.js
+++ assets/js/script.js
@@ -11,6 +11,54 @@ async function infoChicago () {
     document.getElementById('c-wind').textContent = data.current.wind_speed + " ";
     document.getElementById('c-humidity').textContent = data.current.humidity;
     document.getElementById('c-uv').textContent = data.current.uvi;
+
+    document.getElementById('f-temp').textContent = data.current.temp;
+    document.getElementById('f-wind').textContent = data.current.wind_speed + " ";
+    document.getElementById('f-humidity').textContent = data.current.humidity;
+
+    // document.getElementById('s-temp').textContent = data.current.temp;
+    // document.getElementById('s-wind').textContent = data.current.wind_speed + " ";
+    // document.getElementById('s-humidity').textContent = data.current.humidity;
+
+    // document.getElementById('t-temp').textContent = data.current.temp;
+    // document.getElementById('t-wind').textContent = data.current.wind_speed + " ";
+    // document.getElementById('t-humidity').textContent = data.current.humidity;
+
+    // document.getElementById('o-temp').textContent = data.current.temp;
+    // document.getElementById('o-wind').textContent = data.current.wind_speed + " ";
+    // document.getElementById('o-humidity').textContent = data.current.humidity;
+
+    // document.getElementById('i-temp').textContent = data.current.temp;
+    // document.getElementById('i-wind').textContent = data.current.wind_speed + " ";
+    // document.getElementById('i-humidity').textContent = data.current.humidity;
+}
+
+async function infoAustin () {
+
+}
+
+async function infoNewYork () {
+    
+}
+
+async function infoOrlando () {
+    
+}
+
+async function infoSanFrancisco () {
+    
+}
+
+async function infoSeattle () {
+    
+}
+
+async function infoDenver () {
+    
+}
+
+async function infoAtlanta () {
+    
 }
 
 
