ae7f4efebe4c96559f80393f1f63d34f21c9c435
Coder J <buildingguy@sbcglobal.net>
2021-12-20 18:52:44 -0800

    set 'Chicago' button functionality

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,16 @@
 async function infoChicago () {
-    var chicagoWeather = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&&units=imperial&lon=-94.04&exclude=hourly&appid=3f2e409528a52056b62087fa623591ee';
+    var weather = 'https://api.openweathermap.org/data/2.5/onecall?lat=33.44&&units=imperial&lon=-94.04&exclude=hourly&appid=3f2e409528a52056b62087fa623591ee';
 
-    var response = await fetch(chicagoWeather);
+    var response = await fetch(weather);
     var data = await response.json();
     console.log(data);
+    var temp  = data;
+
+    document.getElementById('city').textContent = "Chicago";
+    document.getElementById('c-temp').textContent = data.current.temp;
+    document.getElementById('c-wind').textContent = data.current.wind_speed + " ";
+    document.getElementById('c-humidity').textContent = data.current.humidity;
+    document.getElementById('c-uv').textContent = data.current.uvi;
 }
 
-infoChicago();
 
